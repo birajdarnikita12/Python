cf8579a396f07cf04d9df46f0879311d6b447322
birajdarnikita12 <niksbirajdar12@gmail.com>
2025-05-16 01:13:06 +0530

    Practice set 7

---
+++ Practice Set/CHAPTER 7 - PS/Problem10.py
@@ -0,0 +1,6 @@
+# Write a program to print multiplication table of n using for loops in reversed order.
+n = int(input("Enter a number: "))
+i=10
+while(i>=1):
+    print(f"{n} X i = {n*i}")
+    i-=1
--- Practice Set/CHAPTER 7 - PS/Problem2.py
+++ Practice Set/CHAPTER 7 - PS/Problem2.py
@@ -1,1 +1,9 @@
+# Write a program to greet all the person names stored in a list ‘l’ and which starts with S.
+# l = ["Harry", "Soham", "Sachin", "Rahul"]
+
+l = ["Harry", "Soham", "Sachin", "Rahul"]
+for i in l:
+    if(i.startswith('S')):
+        print(f"Hi {i}")
+
 
